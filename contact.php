8d51fb64dbac9a9883070ed58cf612d4fdad6293
Eri Mehmeti <contact@eridesigns.com.au>
2014-02-07 18:47:04 +1100

    Changing title names

--- contact.php
+++ contact.php
@@ -66,7 +66,7 @@ if(isset($_POST['submit'])) {
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
   
-  <title>Free HTML5 Bronte Theme</title>
+  <title>WordPress Theme for writers</title>
   
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <meta name="description" content="">
